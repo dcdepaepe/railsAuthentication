2471488fdbcb3dbc5ece350fdff635aa09a21735
Devin Cory Depaepe <dcdepaepe@gmail.com>
2015-03-16 13:52:19 -0400

    finished static rails app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def current_date_and_time
+    # Give the current date and time, in the format: "February 6, 2015 at 4:25pm"
+    # Time.now.strftime is a great method chain you can use for this purpose.
+    # See how to use it at: http://apidock.com/ruby/Time/strftime
+
+    Time.now.strftime('%B %d, %Y at %H:%M%P')
+  end
+  
 end
