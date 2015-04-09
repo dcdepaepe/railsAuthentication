684b497962fd753f5a2aa00b55def89c2099d3f2
Devin Cory Depaepe <dcdepaepe@gmail.com>
2015-04-08 20:01:22 -0400

    fixed greater than

--- app/models/spartan_basketball.rb
+++ app/models/spartan_basketball.rb
@@ -1,4 +1,4 @@
 class SpartanBasketball < ActiveRecord::Base
   validates_presence_of :player_name
-  validates :player_number, :numericality => { :greater_than => 1, :less_than => 100 }
+  validates :player_number, :numericality => { :greater_than => 0, :less_than => 100 }
 end
